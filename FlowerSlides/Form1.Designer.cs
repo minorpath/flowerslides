a80bcdb38a12a5ccd91ddad2f979a8e5519a46a8
Henning Mæland <henning.maeland@powel.no>
2014-12-14 21:10:06 +0100

    Initial commit

---
+++ FlowerSlides/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace FlowerSlides
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureTitle = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(389, 211);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureTitle
+            // 
+            this.pictureTitle.AutoSize = true;
+            this.pictureTitle.Font = new System.Drawing.Font("Lucida Sans Unicode", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pictureTitle.Location = new System.Drawing.Point(335, 9);
+            this.pictureTitle.Name = "pictureTitle";
+            this.pictureTitle.Size = new System.Drawing.Size(144, 28);
+            this.pictureTitle.TabIndex = 1;
+            this.pictureTitle.Text = "pictureTitle";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(702, 407);
+            this.Controls.Add(this.pictureTitle);
+            this.Controls.Add(this.pictureBox1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label pictureTitle;
+
+    }
+}
+
---
+++ FlowerSlides/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlowerSlides
+{
+    // TODO: Initial selection of folder to start slideshow from
+
+    // Investigate slides transition
+    // - http://stackoverflow.com/questions/6710631/transition-between-images-for-picturebox-in-c-sharp
+    // - http://stackoverflow.com/questions/3270919/transition-of-images-in-windows-forms-picture-box
+    public partial class Form1 : Form
+    {
+        string _folder = @"C:\Users\hma\Pictures";
+        bool _slideshowStarted = false;
+        List<string> files;
+        int currentIndex = 0;
+
+        // Thumbnails 10px mellom hvert i horisontal retning
+
+        // Farge grå tekst
+        Color dark = Color.FromArgb(29, 29, 29);
+        Color greyText = Color.FromArgb(165, 165, 165);
+        Color lightText = Color.FromArgb(214, 214, 214);
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.BackColor = dark;
+            this.pictureTitle.Visible = false;
+            this.pictureTitle.Top = 40;
+            this.pictureTitle.ForeColor = lightText;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Focus();
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Escape)
+                EndSlideshow();
+        }
+
+        private void StartSlideshow()
+        {
+            StartFullscreen();
+            ShowPicture(files[0]);
+        }
+
+        private void EndSlideshow()
+        {
+            this.pictureTitle.Visible = false;
+            ExitFullscreen();
+            ShowThumbnails();
+        }
+       
+
+        private void ShowThumbnails()
+        {
+//            throw new NotImplementedException();
+        }
+
+        private void StartFullscreen()
+        {
+            FormBorderStyle = FormBorderStyle.None;
+            WindowState = FormWindowState.Maximized;
+        }
+
+        private void ExitFullscreen()
+        {
+            FormBorderStyle = FormBorderStyle.Sizable;
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.F5)
+            {
+                if (_slideshowStarted)
+                    return;
+
+                files = Directory
+                    .EnumerateFiles(_folder) //<--- .NET 4.5
+                    .Where(file => file.ToLower().EndsWith("jpg") || file.ToLower().EndsWith("jpeg"))
+                    .ToList();
+
+                StartSlideshow();
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                NextPicture();
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                PreviousPicture();
+
+            }
+        }
+
+        private void PreviousPicture()
+        {
+            currentIndex--;
+            if (currentIndex == -1)
+                currentIndex = files.Count-1;
+
+            ShowPicture(files[currentIndex]);
+        }
+
+        private void NextPicture()
+        {
+            currentIndex++;
+            if (currentIndex == files.Count)
+                currentIndex = 0;
+
+            ShowPicture(files[currentIndex]);
+        }
+
+        private void ShowPicture(string filename)
+        {
+            var image = Image.FromFile(filename);
+            this.pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            this.pictureBox1.Bounds = this.Bounds;
+            this.pictureBox1.Height -= 80;
+            this.pictureBox1.Top = 80;
+            this.pictureBox1.Image = image;
+
+            this.pictureTitle.Text = FormatPictureTitle(filename);
+            this.pictureTitle.Visible = true;
+        }
+
+        private string FormatPictureTitle(string filename)
+        {
+            return Path.GetFileNameWithoutExtension(filename);
+        }
+
+    }
+}
