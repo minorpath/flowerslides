2da94f741c0b63a7e63d9ce68f78a380856fb45b
Henning Mæland <henning.maeland@powel.no>
2014-12-28 21:50:06 +0100

    Added FormattingTools + tests

---
+++ FlowerSlides.Tests/FormattingToolsTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowerSlides.Tests
+{
+    [TestFixture]
+    public class FormattingToolsTests
+    {
+        [Test]
+        public void FormatPlantName1()
+        {
+            Assert.AreEqual("Penstemon fasciculatus", 
+                FormattingTools.FormatPlantName("4259-penstemon-fasciculatus.jpg"));
+        }
+        [Test]
+        public void FormatPlantName2()
+        {
+            Assert.AreEqual("Penstemon 'Watermelon Taffey'",
+                FormattingTools.FormatPlantName("Penstemon 'Watermelon Taffey'.JPG"));
+        }
+
+        [Test]
+        public void FormatPlantName3()
+        {
+            Assert.AreEqual("Penstemon",
+                FormattingTools.FormatPlantName("Penstemon-001.jpg"));
+        }
+
+    }
+}
---
+++ FlowerSlides/FormattingTools.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowerSlides
+{
+    public static class FormattingTools
+    {
+        public static string FormatPlantName(string name)
+        {
+            // 4259-penstemon-fasciculatus.jpg
+            name = Path.GetFileNameWithoutExtension(name);
+            var tokens = name.Split(new string[] { "-", "_", " " }, 
+                StringSplitOptions.RemoveEmptyEntries);
+
+            string value = "";
+            foreach(string token in tokens)
+            {
+                if( IsNumber(token) )
+                    continue;
+                
+                if( value == "" )
+                {
+                    value += ToTitleCase(token);
+                }
+                else if( token.StartsWith("'") || token.EndsWith("'"))
+                {
+                    value += " " + ToTitleCase(token);
+                }
+                else
+                {
+                    value += " " + token.ToLowerInvariant();
+                }
+            }
+            return value;
+        }
+
+        private static string ToTitleCase(string token)
+        {
+            TextInfo textInfo = CultureInfo.InvariantCulture.TextInfo;
+            return textInfo.ToTitleCase(token);
+        }
+
+        private static bool IsNumber(string token)
+        {
+            int temp = 0;
+            return int.TryParse(token, out temp);
+        }
+    }
+}
