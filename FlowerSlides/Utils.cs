19ef3c36c2fa644e9b36e8c3350f9f902f276a6a
Henning Mæland <henning.maeland@powel.no>
2014-12-29 01:49:22 +0100

    Added utils for getting valid file image files in a folder

---
+++ FlowerSlides/Utils.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace FlowerSlides
+{
+    class Utils
+    {
+        internal static string[] GetValidFilesSorted(string folder)
+        {
+            var validFiles = new List<string>();
+            var validExtensions = new string[] { ".jpg", ".bmp", ".gif", ".png" };
+            var info = new DirectoryInfo(folder);
+
+            foreach (var f in info.GetFiles())
+            {
+                for (var i = 0; i < validExtensions.Length; i++)
+                {
+                    if (f.Extension.ToString().ToLower() == validExtensions[i].ToLower())
+                    {
+                        validFiles.Add(f.FullName.ToLowerInvariant());
+                    }
+                }
+            }
+            validFiles.Sort();
+            return validFiles.ToArray();
+        }
+    }
+}
