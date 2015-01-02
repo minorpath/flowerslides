09f987e68428ff718dbe48e93f9c838892c65aa0
Henning Mæland <henning.maeland@powel.no>
2015-01-02 17:06:39 +0100

    Embed ImageResizer.dll

--- FlowerSlides/Program.cs
+++ FlowerSlides/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -14,9 +16,30 @@ namespace FlowerSlides
         [STAThread]
         static void Main()
         {
+            AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(CurrentDomain_AssemblyResolve);
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+        }
+
+        private static Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            if (args.Name.Contains("ImageResizer"))
+            {
+                // Looking for the Mydll.dll assembly, load it from our own embedded resource
+                foreach (string res in Assembly.GetExecutingAssembly().GetManifestResourceNames())
+                {
+                    if (res.EndsWith("ImageResizer.dll"))
+                    {
+                        Stream s = Assembly.GetExecutingAssembly().GetManifestResourceStream(res);
+                        byte[] buff = new byte[s.Length];
+                        s.Read(buff, 0, buff.Length);
+                        return Assembly.Load(buff);
+                    }
+                }
+            }
+            return null;
         }
     }
 }
