f69fadce1a72b3401df9f3d48d3ad41e859e7325
Henning Mæland <henning.maeland@powel.no>
2015-03-28 21:28:29 +0100

    Fix formatting

--- FlowerSlides.Tests/FormattingToolsTests.cs
+++ FlowerSlides.Tests/FormattingToolsTests.cs
@@ -30,5 +30,58 @@ namespace FlowerSlides.Tests
                 FormattingTools.FormatPlantName("Penstemon-001.jpg"));
         }
 
+        [Test]
+        public void GetLatinName_Standard1()
+        {
+            Assert.AreEqual("Penstemon fasciculatus",
+                FormattingTools.GetLatinName(@"C:\temp\4259-PENSTEMON-FASCICULATUS.jpg"));
+        }
+
+        [Test]
+        public void GetLatinName_HybridWith1Part()
+        {
+            Assert.AreEqual("Penstemon", FormattingTools.GetLatinName(@"C:\temp\Penstemon 'Raven'.jpg"));
+        }
+
+        [Test]
+        public void GetHybridName_HybridWith1Part()
+        {
+            Assert.AreEqual("'Raven'", FormattingTools.GetHybridName(@"C:\temp\Penstemon 'Raven'.jpg"));
+        }
+
+        [Test]
+        public void GetLatinName_HybridWith2Parts()
+        {
+            Assert.AreEqual("Penstemon", FormattingTools.GetLatinName(@"C:\temp\Penstemon_'Plum_Jerkin'.jpg"));
+        }
+
+        [Test]
+        public void GetHybridName_HybridWith2Parts()
+        {
+            Assert.AreEqual("'Plum Jerkin'", FormattingTools.GetHybridName(@"C:\temp\Penstemon_'Plum_Jerkin'.jpg"));
+        }
+
+        [Test]
+        public void GetLatinName_HybridWith3Parts_PenstemonChrisDeBurgh()
+        {
+            Assert.AreEqual("Penstemon", FormattingTools.GetLatinName(@"C:\temp\Penstemon-001 'Chris de Burgh'.jpg"));
+        }
+        [Test]
+        public void GetHybridName_HybridWith3Parts()
+        {
+            Assert.AreEqual("'Chris de Burgh'", FormattingTools.GetHybridName(@"C:\temp\Penstemon-001 'Chris de Burgh'.jpg"));
+        }
+
+        [Test]
+        public void GetLatinName_HybridWith3Parts_ClematisVilleDeLyon()
+        {
+            Assert.AreEqual("Clematis", FormattingTools.GetLatinName(@"C:\temp\Clematis 'Ville de Lyon'.jpg"));
+        }
+
+        [Test]
+        public void GetHybridName_HybridWith3Parts_ClematisVilleDeLyon()
+        {
+            Assert.AreEqual("'Ville de Lyon'", FormattingTools.GetHybridName(@"C:\temp\Clematis 'Ville de Lyon'.jpg"));
+        }
     }
 }
--- FlowerSlides/FormattingTools.cs
+++ FlowerSlides/FormattingTools.cs
@@ -12,25 +12,37 @@ namespace FlowerSlides
     {
         // Penstemon 'Watermelon Taffey' should return Penstemon in italics
         // The 'Watermelon Taffey' part is the Hybrid-name
-        internal static string GetLatinName(string filepath)
+        public static string GetLatinName(string filepath)
         {
             var name = Path.GetFileNameWithoutExtension(filepath);
             var tokens = name.Split(new string[] { "-", "_", " " },
                 StringSplitOptions.RemoveEmptyEntries);
 
             string value = "";
+            bool started = false;
             foreach (string token in tokens)
             {
                 if (IsNumber(token))
                     continue;
 
+                if (started && token.EndsWith("'"))
+                {
+                    started = false;
+                    continue;
+                }
+                if (started)
+                    continue;
+
+                if (!started && token.StartsWith("'"))
+                {
+                    started = true;
+                    continue;
+                }
+
+
                 if (value == "")
                 {
                     value += ToTitleCase(token);
-                }
-                else if (token.StartsWith("'") || token.EndsWith("'"))
-                {
-                    // Ignore
                 }
                 else
                 {
@@ -42,24 +54,32 @@ namespace FlowerSlides
 
         // Penstemon 'Watermelon Taffey' should return 'Watermelon Taffey' in italics
         // The 'Watermelon Taffey' part is the Hybrid-name
-        internal static string GetHybridName(string filepath)
+        public static string GetHybridName(string filepath)
         {
             var name = Path.GetFileNameWithoutExtension(filepath);
             var tokens = name.Split(new string[] { "-", "_", " " },
                 StringSplitOptions.RemoveEmptyEntries);
 
             string value = "";
+            bool started = false;
             foreach (string token in tokens)
             {
                 if (IsNumber(token))
                     continue;
 
-                if (token.StartsWith("'") || token.EndsWith("'"))
+                if (started)
+                    value += " " + token;
+
+                if (token.StartsWith("'"))
                 {
-                    if (value.Length > 0)
-                        value += " ";
-                    value += token;
+                    started = true;
+                    value = token;
+                    continue;
                 }
+
+                if (token.EndsWith("'"))
+                    break;
+
             }
             return value;
         }
@@ -96,7 +116,7 @@ namespace FlowerSlides
         private static string ToTitleCase(string token)
         {
             TextInfo textInfo = CultureInfo.InvariantCulture.TextInfo;
-            return textInfo.ToTitleCase(token);
+            return textInfo.ToTitleCase(token.ToLowerInvariant());
         }
 
         private static bool IsNumber(string token)
